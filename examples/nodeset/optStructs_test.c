2022dad93030fe2cf16d09d8946340251a4bc07f
Grigory Fridman <grigoryfridman@gmail.com>
2020-01-27 14:41:08 +0100

    Tested encoding and decoding structs with optional fields and unions with the RtlsLocationResult type from the AutoID nodeset. Works with other clients like UAExpert too now.

--- examples/nodeset/optStructs_test.c
+++ examples/nodeset/optStructs_test.c
@@ -12,8 +12,14 @@
 #include <open62541/server_config_default.h>
 #endif
 
+/*
 #include "open62541/optstructs_generated_encoding_binary.h"
 #include "open62541/optstructs_generated_handling.h"
+*/
+
+#include "open62541/types_autoid_generated_encoding_binary.h"
+#include "open62541/types_autoid_generated_handling.h"
+
 
 #include <signal.h>
 #include <stdlib.h>
@@ -33,7 +39,50 @@ int main(int argc, char** argv) {
     UA_Server *server = UA_Server_new();
     UA_ServerConfig_setDefault(UA_Server_getConfig(server));
 
+    UA_RtlsLocationResult *res = UA_RtlsLocationResult_new();
+    res->hasLocation = true;
+    res->codeType = UA_STRING("RAW:BYTES");
+    res->scanData.switchField = 1;
+    res->scanData.byteString = UA_BYTESTRING("02-AD-BC-78");
+    res->timestamp = UA_DateTime_now();
+    res->location.switchField = 2;
+    res->location.local.x = 1300.4;
+    res->location.local.y = 789.3;
+    res->location.local.z = 267.83;
+    res->speed = 289.44;
+    res->heading = 23.839;
+    res->rotation.pitch = 35.6;
+    res->rotation.roll = 46.7;
+    res->rotation.yaw = 96.2;
+    res->receiveTime = UA_DateTime_now();
 
+    UA_ByteString *resBuf = UA_ByteString_new();
+    size_t resSize = UA_RtlsLocationResult_calcSizeBinary(res);
+    UA_ByteString_allocBuffer(resBuf, resSize);
+    memset(resBuf->data, 0, resSize);
+    UA_Byte *resBufPos = resBuf->data;
+    const UA_Byte *resBufEnd = &resBuf->data[resBuf->length];
+     UA_RtlsLocationResult_encodeBinary(res, &resBufPos, resBufEnd);
+
+    UA_RtlsLocationResult *resD = UA_RtlsLocationResult_new();
+    size_t resOffset = 0;
+    UA_RtlsLocationResult_decodeBinary(resBuf, &resOffset, resD);
+
+    printf("hasLocation: %d\n", resD->hasLocation);
+    printf("codeType: %s\n", resD->codeType.data);
+    printf("sd switchField: %d\n", resD->scanData.switchField);
+    printf("sd byteString: %s\n", resD->scanData.byteString.data);
+    printf("timestamp: %ld\n", resD->timestamp);
+    printf("location switchField: %d\n", resD->location.switchField);
+    printf("location local x: %f\n", resD->location.local.x);
+    printf("location local y: %f\n", resD->location.local.y);
+    printf("location local z: %f\n", resD->location.local.z);
+    printf("speed: %f\n", resD->speed);
+    printf("heading: %f\n", resD->heading);
+    printf("rotation pitch: %f\n", resD->rotation.pitch);
+    printf("receivetime: %ld\n", resD->receiveTime);
+
+/*
     UA_Location* l = UA_Location_new();
     l->switchField = 0;
     l->local = UA_STRING("local");
@@ -85,7 +134,6 @@ int main(int argc, char** argv) {
 
     UA_NotOptStruct_encodeBinary(r, &bufSettingsPos, bufEnd);
 
-/*--------------------------------------------------------------------*/
 
     UA_OptStruct* o = UA_OptStruct_new();
 
@@ -122,7 +170,7 @@ int main(int argc, char** argv) {
     printf("antennaName: %s\n", od->antennaName.data);
     printf("testFloat: %f\n", od->testFloat);
     printf("testDouble: %f\n", od->testDouble);
-
+*/
     UA_StatusCode retval;
 
     retval = UA_Server_run(server, &running);
